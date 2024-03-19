34581378757500b5b9ffde2676392e49993fb6e7
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-19 21:28:27 +0530

    feat: Add firebase connection to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sealtech/client/navbar.dart';
 import 'package:sealtech/components/theme.dart';
+import 'package:sealtech/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
