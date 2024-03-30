33211855a14ceb995991d9956ae222f96c7d46ad
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-30 19:50:29 +0530

    refactor: Try fixing invite friends backend

--- lib/client/profile/inviteFriends.dart
+++ lib/client/profile/inviteFriends.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 import 'package:sealtech/components/button.dart';
 import 'package:sealtech/components/theme.dart';
+import 'package:sealtech/services/auth/auth_service.dart';
 
 class InviteFriends extends StatefulWidget {
   @override
@@ -10,11 +14,51 @@ class InviteFriends extends StatefulWidget {
 class _InviteFriendsState extends State<InviteFriends> {
   final _formKey = GlobalKey<FormState>();
   String _friendEmail = '';
+  String _userEmail = ''; // User's email obtained from AuthService
 
-  void _submit() {
+  AuthService _authService = AuthService(); // Instance of AuthService
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUserEmail(); // Call method to fetch current user's email
+  }
+
+  // Method to fetch current user's email
+  void _getCurrentUserEmail() async {
+    User? userData = await _authService.getCurrentUser();
+    setState(() {
+      _userEmail = userData?.email ?? ''; // Set user's email
+    });
+  }
+
+  void _submit() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      // backend logic
+      
+      // Email content
+      String subject = 'Invitation to Join';
+      String body = 'Hi there! I invite you to join our app.';
+      
+      // Configure SMTP server details
+      final smtpServer = gmail(_userEmail, 'your_password'); // Replace with sender's email and password
+      
+      // Create message
+      final message = Message()
+        ..from = Address(_userEmail)
+        ..recipients.add(_friendEmail)
+        ..subject = subject
+        ..text = body;
+      
+      // Send email
+      try {
+        await send(message, smtpServer);
+        // Email sent successfully
+        print('Email sent successfully');
+      } catch (e) {
+        // Failed to send email
+        print('Error sending email: $e');
+      }
     }
   }
 
@@ -58,7 +102,7 @@ class _InviteFriendsState extends State<InviteFriends> {
                 SizedBox(height: 32),
                 Button(
                   buttonText: 'Submit',
-                  onPressed: () {},
+                  onPressed: _submit,
                   color: 'orange',
                   enableIcon: false,
                 )
