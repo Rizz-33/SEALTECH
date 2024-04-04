3bd428b86c9f3047cdae156f3979719f1d78ac98
Hiru1003 <hirumiperera10@gmail.com>
2024-04-04 09:10:43 +0530

    feat: search function

--- lib/client/search.dart
+++ lib/client/search.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/client/product.dart';
 import 'package:sealtech/components/theme.dart';
+import 'package:sealtech/client/product.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -8,153 +8,159 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-  List<String> additionalRows = [
-    'Additional Row 1',
-    'Additional Row 2',
-    'Additional Row 3',
-    'Additional Row 4',
-    'Additional Row 5',
+  List<ProductPage> products = [
+    const ProductPage(
+        imagePath: 'lib/images/Pool Waterproofing.jpeg',
+        title: 'Pool Waterproofing',
+        subtitle: 'Service',
+        price: '3 million LKR +'),
+    const ProductPage(
+        imagePath: 'lib/images/Roof Waterproofing.jpeg',
+        title: 'Roof Waterproofing',
+        subtitle: 'Service',
+        price: '1 million LKR +'),
+    const ProductPage(
+        imagePath: 'lib/images/Basement Sealing.jpeg',
+        title: 'Basement Sealing',
+        subtitle: 'Service',
+        price: '1.5 million LKR +'),
+    const ProductPage(
+        imagePath: 'lib/images/Side walls Sealing.jpeg',
+        title: 'Side walls Sealing',
+        subtitle: 'Service',
+        price: '2 million LKR +'),
+    const ProductPage(
+        imagePath: 'lib/images/Concrete Sealing.jpeg',
+        title: 'Concrete Sealing',
+        subtitle: 'Service',
+        price: '1 million LKR +'),
+    const ProductPage(
+        imagePath: 'lib/images/Drill Machine.jpeg',
+        title: 'Drill Machine',
+        subtitle: 'Service',
+        price: '9 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Screwdriver Set.jpg',
+        title: 'Screwdriver Set',
+        subtitle: 'Service',
+        price: '12 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Circular Saw.jpeg',
+        title: 'Circular Saw',
+        subtitle: 'Service',
+        price: '5 000 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Adjustable Wrench.jpg',
+        title: 'Adjustable Wrench',
+        subtitle: 'Service',
+        price: '7 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Pipe Wrench.jpeg',
+        title: 'Pipe Wrench',
+        subtitle: 'Service',
+        price: '3 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Wood Protector.jpeg',
+        title: 'Wood Protector',
+        subtitle: 'Service',
+        price: '9 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Waterproof Sealant.png',
+        title: 'Waterproof Sealant',
+        subtitle: 'Service',
+        price: '12 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Surface Cleaner.jpeg',
+        title: 'Surface Cleaner',
+        subtitle: 'Service',
+        price: '5 000 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Mold Inhibitor Spray.jpeg',
+        title: 'Mold Inhibitor Spray',
+        subtitle: 'Service',
+        price: '7 500 LKR'),
+    const ProductPage(
+        imagePath: 'lib/images/Wood Protector.jpeg',
+        title: 'Wood Protector',
+        subtitle: 'Service',
+        price: '9 500 LKR'),
   ];
 
-  List<String> originalRows = [
-    'Additional Row 1',
-    'Additional Row 2',
-    'Additional Row 3',
-    'Additional Row 4',
-    'Additional Row 5',
-  ];
+  List<ProductPage> filteredProducts = [];
 
-  bool isCleared = false;
+  @override
+  void initState() {
+    filteredProducts = products;
+    super.initState();
+  }
 
-  void removeRow(int index) {
+  void filterProducts(String query) {
     setState(() {
-      additionalRows.removeAt(index);
+      if (query.isEmpty) {
+        filteredProducts = products;
+      } else {
+        filteredProducts = products
+            .where((product) =>
+                product.title.toLowerCase().contains(query.toLowerCase()) ||
+                product.subtitle.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
     });
   }
 
-  void removeAllRows() {
-    setState(() {
-      additionalRows.clear();
-      isCleared = true;
-    });
-  }
-
-  void showAllRows() {
-    setState(() {
-      additionalRows = List.from(originalRows);
-      isCleared = false;
-    });
-  }
-
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 100,
-          title: Center(
-            child: Image.asset('lib/images/logo-word-no-background.png', width: 160,),
-          ),
-          backgroundColor: bgColor,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: TextField(
-                  cursorColor: accent75,
-                  decoration: InputDecoration(
-                    hintText: 'Search by Names, Categories or Keywords',
-                    hintStyle: const TextStyle(fontSize: 14),
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: secondaryColor,
-                    contentPadding: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 16),
-                    child: Text(
-                      'Popular Searches',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: isCleared ? showAllRows : removeAllRows,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 16),
-                      child: Text(
-                        isCleared ? 'Show All' : 'Clear All',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(fontWeight: FontWeight.bold, color: primaryColor),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: additionalRows.length,
-                itemBuilder: (context, index) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16),
-                        child: Text(
-                          additionalRows[index],
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.close, size: 18,),
-                        onPressed: () {
-                          removeRow(index);
-                        },
-                      ),
-                    ],
-                  );
-                },
-              ),
-              const SizedBox(height: 16,),
-              const SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.only(left: 6),
-                child: Row(
-                  children: [
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8,),
-              const SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.only(left: 6),
-                child: Row(
-                  children: [
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                    ProductPage(imagePath: 'lib/images/pro1.png', title: 'Swimming Pool\n(8ft)', subtitle: 'Service', price: '2 million LKR +'),
-                  ],
-                ),
-              ),
-            ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Center(
+          child: Image.asset(
+            'lib/images/logo-word-no-background.png',
+            width: 160,
           ),
         ),
-      );
+        backgroundColor: bgColor,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              cursorColor: accent75,
+              onChanged: filterProducts,
+              decoration: InputDecoration(
+                hintText: 'Search by Names, Categories or Keywords',
+                hintStyle: const TextStyle(fontSize: 14),
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: secondaryColor,
+                contentPadding: const EdgeInsets.symmetric(vertical: 8),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredProducts.length,
+              itemBuilder: (context, index) {
+                final product = filteredProducts[index];
+                return ListTile(
+                  title: Text(product.title),
+                  subtitle: Text(product.subtitle),
+                  trailing: Text(product.price),
+                  leading: Image.asset(product.imagePath),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
