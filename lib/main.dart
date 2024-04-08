ec8f6189192a374886fad0c36e57cc7937e7ca91
Hiru1003 <hirumiperera10@gmail.com>
2024-04-08 18:48:53 +0530

    modify: main back to authgate

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sealtech/Employee/navbarE.dart';
 import 'package:sealtech/client/models/product.dart';
 import 'package:sealtech/components/theme.dart';
 import 'package:sealtech/firebase_options.dart';
@@ -34,7 +33,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: NavbarE(),
+      home: AuthGate(),
     );
   }
 }
