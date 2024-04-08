0439ef59d55b11ebca85875bb6b1d28588c10088
Olitha Balasuriya <olibala246@gmail.com>
2024-04-08 09:04:10 +0530

    employeebackend

---
+++ lib/employee2/DataPage.dart
@@ -0,0 +1,191 @@
+
+import 'package:cny_det/pages/appointment.dart';
+import 'package:cny_det/service/database.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:random_string/random_string.dart';
+
+class DataPage extends StatefulWidget {
+  @override
+  _DataPageState createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController deadlineController = TextEditingController(); 
+  TextEditingController addressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 231, 150, 20),
+        title: Align(
+          alignment: Alignment.center,
+          child: Text(
+            "new sites",
+            textAlign: TextAlign.left,
+          ),
+        ),
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25),
+            bottomRight: Radius.circular(25),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  width: 250,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: Image(
+                    image: AssetImage('assets/download.jpg'),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50.0),
+              Text(
+                "Name",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                "Deadline", 
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: deadlineController, 
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                "Address",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: addressController,
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    String Id = randomAlphaNumeric(10);
+                    Map<String, dynamic> employeeInfoMap = {
+                      "Name": nameController.text,
+                      "Deadline": deadlineController.text,
+                      "Id": Id,
+                      "Address": addressController.text,
+                    };
+                    await DatabaseMethods()
+                        .addEmployeeDetails(employeeInfoMap, Id)
+                        .then((value) {
+                      Fluttertoast.showToast(
+                        msg: "Details added successfully",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.blue,
+                        textColor: Colors.white,
+                        fontSize: 16.0,
+                      );
+                    });
+                  },
+                  child: const Text(
+                    "Submit",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Dismissible(
+                key: Key('appointment_card'),
+                direction: DismissDirection.startToEnd,
+                onDismissed: (direction) {
+                  // Navigate to another page here
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => appointmentp()),
+                  );
+                },
+                background: Container(
+                  color: Colors.green,
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  alignment: Alignment.centerLeft,
+                  child: Icon(Icons.arrow_forward),
+                ),
+                child: Card(
+                  elevation: 5,
+                  child: ListTile(
+                    title: Text('Appointments'),
+                    subtitle: Text('Slide to view appointments'),
+                    leading: Icon(Icons.calendar_today),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/employee2/sites.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class UserDetailsPage extends StatefulWidget {
+  final String name;
+  final String deadline;
+  final String address;
+  final double duePayment;
+
+  UserDetailsPage({
+    required this.name,
+    required this.deadline,
+    required this.address,
+    required this.duePayment,
+  });
+
+  @override
+  State<UserDetailsPage> createState() => _UserDetailsPageState();
+}
+
+class _UserDetailsPageState extends State<UserDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Ongoing Locations',
+                style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  color: const Color.fromARGB(255, 70, 66, 68),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        titleSpacing: 4.0,
+        toolbarHeight: 65,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(18),
+            bottomLeft: Radius.circular(18),
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        elevation: 0.00,
+        backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20, // Add a gap here
+            ),
+            SizedBox(
+              width: 404,
+              height: 213,
+              child: Image.network(
+                'https://www.europarl.europa.eu/resources/library/images/20180612PHT05623/20180612PHT05623-ml.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 206, 134, 34),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Name: ${widget.name}',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Deadline: ${widget.deadline}',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Address: ${widget.address}',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Due Payment: \$${widget.duePayment.toStringAsFixed(2)}',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Progress',
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  MyCheckBoxContainer(title: 'Visit Site'),
+                  const SizedBox(height: 10),
+                  MyCheckBoxContainer(title: 'Give Quotation'),
+                  const SizedBox(height: 10),
+                  MyCheckBoxContainer(title: 'Start Work'),
+                  const SizedBox(height: 10),
+                  MyCheckBoxContainer(title: 'Completion'),
+                  const SizedBox(height: 15),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCheckBoxContainer extends StatefulWidget {
+  final String title;
+
+  MyCheckBoxContainer({required this.title});
+
+  @override
+  _MyCheckBoxContainerState createState() => _MyCheckBoxContainerState();
+}
+
+class _MyCheckBoxContainerState extends State<MyCheckBoxContainer> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isChecked = !isChecked;
+        });
+      },
+      child: Container(
+        width: 330,
+        height: 45,
+        padding: const EdgeInsets.only(left: 15),
+        decoration: BoxDecoration(
+          color: Colors.white, // Change to your desired background color
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.black, // Change to your desired border color
+          ),
+        ),
+        child: Row(
+          children: [
+            const SizedBox(width: 25),
+            Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: isChecked ? Colors.black : Colors.transparent,
+                  width: 2.0,
+                ),
+              ),
+              child: CircleAvatar(
+                radius: 15,
+                backgroundColor: isChecked ? Colors.black : Colors.white,
+                child: isChecked
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: 15.0,
+                      )
+                    : null,
+              ),
+            ),
+            const SizedBox(width: 30),
+            Text(
+              widget.title,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Color.fromARGB(255, 96, 94, 94),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
